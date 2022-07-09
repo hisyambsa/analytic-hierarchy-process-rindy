8ce266ff6eb134506f47bbcef2d88c9d731301bf
Hisyam <36149214+hisyambsa@users.noreply.github.com>
2022-07-09 11:04:11 +0700

    install math-php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '55a4b36c38242d5b12fbe2acfebe7b2a481df3a0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '55a4b36c38242d5b12fbe2acfebe7b2a481df3a0',
+    ),
+    'markrogoyski/math-php' => 
+    array (
+      'pretty_version' => 'v2.6.0',
+      'version' => '2.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '85d7d7fe205a6df2b20f956720e25341f3b1462a',
+    ),
+  ),
+);
